b674a4ca266dc59c4cac888aac14939c09c2aaac
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-28 13:27:56 +0000

    reduced the quantity of licences, because there are too many to render

---
+++ README.md
@@ -0,0 +1,35 @@
+# kdndkj
+
+
+## Table of Contents
+* [Description](#description) 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Description
+alsdkjladjda
+
+## Installation
+akdhakhdakjads
+
+## Usage
+akdhdakdhaskdasj
+
+## License
+MIT License
+
+## Usage
+aksdhakdsjhas
+
+## Tests
+kadsjhasdkhadksj
+
+## Questions
+kadsjhakdsjhd
+
+
+kadsjhkdasjhdsik
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 inquirer.prompt([{
         type: "input",
@@ -20,11 +21,7 @@ inquirer.prompt([{
     },{
         type:"list",
         name: "License",
-        choices: ["N/A", "Apache License 2.0", "GNU General Public License v3.0",
-                "MIT License", "BSD 2-Clause License", "BSD 3-Clause License", 
-                "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", 
-                "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", 
-                "Mozilla Public License 2.0", "The Unlicense"],
+        choices: ["N/A", "Apache License 2.0", "GNU General Public License v3.0", "MIT License"],
         message:"please choose a license?",
     },{
         type:"input",
